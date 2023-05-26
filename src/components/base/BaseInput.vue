b523c5f7218008d074ceef389e699d6e08d73f9a
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-05-26 16:32:08 +0200

    :sparkle: Component setup

---
+++ src/components/base/BaseInput.vue
@@ -0,0 +1,35 @@
+<template>
+	<label> {{ label }} </label>
+	<input
+		v-bind="$attrs"
+		:placeholder="label"
+		:value="modelValue"
+		@input="emitToParent"
+	/>
+</template>
+
+<script setup lang="ts">
+	const emits = defineEmits(['update:modelValue']);
+
+	defineProps({
+		label: {
+			type: String,
+			default: '',
+		},
+		modelValue: {
+			type: [String, Number] || null,
+			default: '',
+		},
+	});
+
+	const emitToParent = (event: Event) => {
+		const thisInputValue = (event.target as HTMLInputElement).value;
+		emits('update:modelValue', thisInputValue);
+	};
+</script>
+
+<style scoped>
+	input {
+		margin-block-end: 1rem;
+	}
+</style>

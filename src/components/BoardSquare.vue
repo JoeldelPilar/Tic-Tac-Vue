70705e2daf6996cef19c16ae7a32de822a7dda38
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-06-01 09:41:39 +0200

    :sparkle: Refactoring logic for game

---
+++ src/components/BoardSquare.vue
@@ -0,0 +1,54 @@
+<template>
+	<button
+		@click="handleClick"
+		:disabled="gotSelected"
+	>
+		<span>{{ marker }}</span>
+	</button>
+</template>
+
+<script setup lang="ts">
+	import { ref, toRef, watch } from 'vue';
+
+	const emits = defineEmits(['changePlayer']);
+
+	const props = defineProps({
+		index: Number,
+		reset: Boolean,
+		player: Object,
+	});
+
+	const id = props.index;
+	const gotSelected = ref(false);
+	const marker = ref('');
+	const reset = toRef(props.reset);
+
+	watch(reset, (isReseted, isNotReseted) => {
+		console.log(isNotReseted, isReseted);
+		if (isReseted) {
+			marker.value = '';
+		}
+	});
+
+	const handleClick = () => {
+		marker.value = props.player?.marker;
+		gotSelected.value = true;
+		emits('changePlayer', id, marker);
+	};
+</script>
+
+<style scoped>
+	button {
+		position: relative;
+	}
+
+	button:disabled {
+		color: hsl(153, 47%, 49%);
+	}
+	span {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+</style>
--- src/models/GameSquare.ts
+++ src/models/GameSquare.ts
@@ -1,6 +1,14 @@
 export class Square {
   constructor(
-    public playerMarker: string,
+    public marker: string,
     public isUsed: boolean,
+    public index: number,
+  ){}
+}
+
+export class PlayedSquare {
+  constructor(
+    public marker: string,
+    public id: number,
   ){}
 }
